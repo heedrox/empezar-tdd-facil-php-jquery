9cef2982bff20615072da59a377cc7325474639e
jmarti <jmarti@theinit.com>
2017-04-06 23:43:22 +0200

    añadir la solución final

--- tests-js/capitulo4/calcularImportes.test.js
+++ tests-js/capitulo4/calcularImportes.test.js
@@ -21,6 +21,54 @@ describe("calcularImportes.js calcula los importes por cada producto y agrega un
         var salida = calcularImportes(entrada);
 
         //THEN
-        expect(salida[0]).toEqual({ id: 1, title: "Maniac Mansion Videojuego", ctd: 5, pvp: 40, importe: 200 });
+        expect(salida[0].id).toBe(1);
+        expect(salida[0].title).toBe("Maniac Mansion Videojuego");
+        expect(salida[0].ctd).toBe(5);
+        expect(salida[0].pvp).toBe(40);
+        expect(salida[0].importe).toBe(200);
+
+    });
+
+    it("calcula el importe con impuestos para un producto como el importe para un producto * 1,21", function() {
+        //GIVEN
+        var entrada = [ { id: 1, title: "Maniac Mansion Videojuego", ctd: 5, pvp: 40 } ];
+
+        //WHEN
+        var salida = calcularImportes(entrada);
+
+        //THEN
+        expect(salida[0].importeImp).toBe(242);
+    })
+
+    it("calcula el importe total en la última línea, con id 0", function() {
+        //GIVEN
+        var entrada = [ { id: 1, title: "Maniac Mansion Videojuego", ctd: 5, pvp: 40 } ];
+
+        //WHEN
+        var salida = calcularImportes(entrada);
+
+        //THEN
+        expect(salida[1]).toEqual( { id: 0, importe: 200, importeImp: 242 });
+    });
+
+    it("calcula los importes para varios productos", function() {
+        //GIVEN
+        var entrada = [
+            { id: 1, title: "Maniac Mansion Videojuego", ctd: 5, pvp: 40 },
+            { id: 2, title: "Consola XBOX Zero", ctd: 1, pvp: 300 },
+            { id: 3, title: "Prison Break Temporada 76", ctd: 2, pvp: 15 }
+            ];
+
+        //WHEN
+        var salida = calcularImportes(entrada);
+
+        //THEN
+        expect(salida).toEqual(
+            [
+                { id: 1, title: "Maniac Mansion Videojuego", ctd: 5, pvp: 40, importe: 200, importeImp: 242  },
+                { id: 2, title: "Consola XBOX Zero", ctd: 1, pvp: 300, importe: 300, importeImp: 363 },
+                { id: 3, title: "Prison Break Temporada 76", ctd: 2, pvp: 15, importe: 30, importeImp: 36.30 },
+                { id: 0, importe: 530, importeImp: 641.30 }
+            ] );
     });
 });
--- web/js/capitulo4/calcularImportes.js
+++ web/js/capitulo4/calcularImportes.js
@@ -1,15 +1,31 @@
 function obtenerSalida() {
-    var entrada = $('#entrada').val();
-    $('#salida').val(calcularImportes(entrada));
+    var entrada = JSON.parse($('#entrada').val());
+    $('#salida').val(JSON.stringify(calcularImportes(entrada)));
 }
 
 function calcularImportes(entrada) {
     var entradaConImportes = entrada.map(aniadirImporte);
-    entradaConImportes.push({ id: 0, importe: 0, importeImp: 0 });
-    return entradaConImportes;
+    var entradaConImportesImpuestos = entradaConImportes.map(aniadirImporteImpuestos);
+    entradaConImportesImpuestos.push(calcularRegistroTotal(entradaConImportesImpuestos));
+    return entradaConImportesImpuestos;
 
-    function aniadirImporte(pedido) {
-        pedido.importe = pedido.ctd * pedido.pvp;
-        return pedido;
+    function aniadirImporte(producto) {
+        producto.importe = producto.ctd * producto.pvp;
+        return producto;
     }
+
+    function aniadirImporteImpuestos(producto) {
+        producto.importeImp = producto.importe * 1.21;
+        return producto;
+    }
+    function calcularRegistroTotal(productos) {
+        var sumaProductos = productos.reduce(suma, { importe: 0, importeImp: 0 });
+        return { id: 0, importe: sumaProductos.importe, importeImp: sumaProductos.importeImp };
+
+        function suma(a, b) {
+            return { importe: a.importe + b.importe, importeImp: a.importeImp + b.importeImp };
+        }
+    }
+
 }
+
