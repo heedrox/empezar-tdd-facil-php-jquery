a1e1c99fd3e8e6fca40cadea36c6201817fed647
jmarti <jmarti@theinit.com>
2017-04-08 17:15:44 +0200

    arreglar el test del capitulo 5

--- tests-js/capitulo5/validarFormulario.test.js
+++ tests-js/capitulo5/validarFormulario.test.js
@@ -1,7 +1,9 @@
 describe("validarFormulario valida el formulario del capitulo 5", function() {
     it("muestra mensaje error dado un formulario vacio", function() {
+        jasmine.getFixtures().fixturesPath = 'base/templates';
+        loadFixtures("capitulo5.html.twig");
         enviarFormulario();
 
-        $('#mensajeValidacion').not.toBeHidden();
+        expect($('#mensajeValidacion')).toBeHidden();
     });
 });
