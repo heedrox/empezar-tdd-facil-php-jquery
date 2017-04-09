6646fa76ebb885e40aa1aa15b43df57e93a998f9
jmarti <jmarti@theinit.com>
2017-04-09 11:52:15 +0200

    tests finales del capitulo 5

--- tests-js/capitulo5/validarFormulario.test.js
+++ tests-js/capitulo5/validarFormulario.test.js
@@ -15,7 +15,7 @@ describe("validarFormulario valida el formulario del capitulo 5", function() {
     });
 
     describe("validación incorrecta", function() {
-        var camposQueCausanValidacionIncorrecta = [ "email", "fechaNacimiento", "paquete"];
+        var camposQueCausanValidacionIncorrecta = [ "email", "fechaNacimiento", "paquete", "numMensualidades", "tarjetaVisa"];
 
         using(camposQueCausanValidacionIncorrecta, function(campo) {
             it("muestra mensaje error si "+campo+" no rellenado", function() {
@@ -28,10 +28,26 @@ describe("validarFormulario valida el formulario del capitulo 5", function() {
                 expect($('#textoValidacion').html()).toBe(campo);
             });
         });
+
+        it("muestra mensaje error si el checkbox de condiciones no está chequeado", function() {
+            dadoUnFormularioRellenadoCorrectamente();
+            $('#condiciones').prop("checked", false);
+
+            enviarFormulario();
+
+            expect($('#mensajeValidacion')).not.toBeHidden();
+            expect($('#textoValidacion').html()).toBe("condiciones");
+        });
+
     });
+
+
     function dadoUnFormularioRellenadoCorrectamente() {
         $('#email').val("unemail@cualquiera.com");
         $('#fechaNacimiento').val(new Date());
         $('#paquete').val("BASICO");
+        $('#numMensualidades').val(12);
+        $('#tarjetaVisa').val("1234123412341234");
+        $('#condiciones').prop("checked", true);
     }
 });
--- web/js/capitulo5/validarFormulario.js
+++ web/js/capitulo5/validarFormulario.js
@@ -1,14 +1,37 @@
 function enviarFormulario() {
-    if ($('#email').val() == "") {
-        $('#mensajeValidacion').show();
-        $('#textoValidacion').html("email");
-    } else if ($('#fechaNacimiento').val() == "") {
-        $('#mensajeValidacion').show();
-        $('#textoValidacion').html("fechaNacimiento");
-    } else if ($('#paquete').val() == "") {
-        $('#mensajeValidacion').show();
-        $('#textoValidacion').html("paquete");
-    } else {
-        $('#mensajeValidacion').hide();
+    var camposAValidar = ["email", "fechaNacimiento", "paquete", "numMensualidades", "tarjetaVisa"];
+    var objForm = $('form').serializeArray();
+    var campoErroneo = validarFormulario(objForm, camposAValidar);
+    campoErroneo = campoErroneo!=""?campoErroneo:validarCondiciones($('#condiciones').prop("checked"));
+    procesarCampoErroneo(campoErroneo);
+
+    function procesarCampoErroneo(campoErroneo) {
+        if (campoErroneo == "") {
+            $('#mensajeValidacion').hide();
+        } else {
+            $('#mensajeValidacion').show();
+            $('#textoValidacion').html(campoErroneo);
+        }
+    }
+
+    function validarCondiciones(ischecked) {
+        return ischecked?"":"condiciones";
+    }
+}
+
+function validarFormulario(objForm, campos) {
+    for (var a = 0;a<campos.length;a++) {
+        var valorCampo = obtenerValorCampo(objForm, campos[a]);
+        if (valorCampo == "") {
+            return campos[a];
+        }
+    }
+    return "";
+
+    function obtenerValorCampo(objForm, campo) {
+        for (var a=0;a<objForm.length;a++) {
+            if (objForm[a].name == campo) return objForm[a].value;
+        }
+        return "";
     }
 }
