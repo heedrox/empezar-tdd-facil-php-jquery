1178f44c0f49f7d6a66fc2dd6d215a29fd59b467
jmarti <jmarti@theinit.com>
2017-04-09 10:50:35 +0200

    refactor multiple fields without data provider

--- tests-js/capitulo5/validarFormulario.test.js
+++ tests-js/capitulo5/validarFormulario.test.js
@@ -1,9 +1,38 @@
 describe("validarFormulario valida el formulario del capitulo 5", function() {
-    it("muestra mensaje error dado un formulario vacio", function() {
+    beforeEach(function() {
         jasmine.getFixtures().fixturesPath = 'base/templates';
         loadFixtures("capitulo5.html.twig");
-        enviarFormulario();
+    });
 
-        expect($('#mensajeValidacion')).toBeHidden();
+    describe("validación correcta", function() {
+        it("no muestra mensaje error si todos los campos están bien rellenados", function() {
+            dadoUnFormularioRellenadoCorrectamente();
+
+            enviarFormulario();
+
+            expect($('#mensajeValidacion')).toBeHidden();
+        });
+    });
+
+    describe("validación incorrecta", function() {
+        var camposQueCausanValidacionIncorrecta = [ "email", "fechaNacimiento"];
+
+        camposQueCausanValidacionIncorrecta.forEach(function(campo) {
+            it("muestra mensaje error si "+campo+" no rellenado", function() {
+                dadoUnFormularioRellenadoCorrectamente();
+                $('#'+campo).val('');
+
+                enviarFormulario();
+
+                expect($('#mensajeValidacion')).not.toBeHidden();
+                expect($('#textoValidacion').html()).toBe(campo);
+            });
+        });
+
     });
 });
+
+function dadoUnFormularioRellenadoCorrectamente() {
+    $('#email').val("unemail@cualquiera.com");
+    $('#fechaNacimiento').val(new Date());
+}
--- web/js/capitulo5/validarFormulario.js
+++ web/js/capitulo5/validarFormulario.js
@@ -1,3 +1,11 @@
 function enviarFormulario() {
-
+    if ($('#email').val() == "") {
+        $('#mensajeValidacion').show();
+        $('#textoValidacion').html("email");
+    } else if ($('#fechaNacimiento').val() == "") {
+        $('#mensajeValidacion').show();
+        $('#textoValidacion').html("fechaNacimiento");
+    } else {
+        $('#mensajeValidacion').hide();
+    }
 }
